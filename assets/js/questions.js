159200679f5077600e90aedc02cdc6a9cfd1def6
henrysevern <henrysevern2@gmail.com>
2022-02-28 21:44:50 +0000

    Add new js file for quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+question: "At which bridge does the annual Oxford and Cambridge boat race start?",
+answers: [
+    {text: "Putney", correct: true},
+    {text: "Hammersmith", correct: false},
+{text: "Vauxhall ", correct: false},
+{text: "Battersea", correct: false},
+]
+    }
+]
