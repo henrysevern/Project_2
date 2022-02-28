84dd358ac375e08b75445bf658efd6a4c27756b7
henrysevern <henrysevern2@gmail.com>
2022-02-28 22:37:57 +0000

    Add functions for questions and answers, correct and wrong answers, and restart

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,19 +1,79 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffleQuestions, currentQuestionIndex
+
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
 startButton.classList.add('hide')
+shuffleQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
 questionContainerElement.classList.remove('hide')
 setNextQuestion()
 }
 
 function setNextQuestion() {
-
+    resetState()
+ showQuestion(shuffleQuestions[currentQuestionIndex])
+ 
 }
 
-function selectAnswer() {
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+     const selectedButton = e.target
+     const correct = selectedButton.dataset.correct
+     setStatusClass(document.body, correct)
+     Array.from(answerButtonsElement.children).forEach(button => {
+         setStatusClass(button, button.dataset.correct)
+     })
+     if (shuffleQuestions.length > currentQuestionIndex + 1) {
+     nextButton.classList.remove('hide')
+     }else {
+         startButton.innerText = 'Restart'
+         startButton.classList.remove('hide')
+     }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else 
+    element.classList.add('incorrect')
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
 
 }
 
