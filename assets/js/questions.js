490754de097391630540a347a8210a31918f4ef7
henrysevern <henrysevern2@gmail.com>
2022-03-03 12:01:03 +0000

    Add sports questions to questios.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,11 +1,125 @@
-const questions = [
-    {
-question: "At which bridge does the annual Oxford and Cambridge boat race start?",
-answers: [
-    {text: "Putney", correct: true},
-    {text: "Hammersmith", correct: false},
-{text: "Vauxhall ", correct: false},
-{text: "Battersea", correct: false},
-]
-    }
+const questions = [{
+    question: "At which bridge does the annual Oxford and Cambridge boat race start?",
+    answers: [{
+            text: "Putney",
+            correct: true
+        },
+        {
+            text: "Hammersmith",
+            correct: false
+        },
+        {
+            text: "Vauxhall ",
+            correct: false
+        },
+        {
+            text: "Battersea",
+            correct: false
+        },
+    ]
+},
+
+{
+    question: "With which team did Michael Schumacher make his Formula One debut at the 1991 Belgian Grand Prix?",
+    answers: [{
+            text: "Jordan",
+            correct: true
+        },
+        {
+            text: "Benetton",
+            correct: false
+        },
+        {
+            text: "Ferrari",
+            correct: false
+        },
+        {
+            text: "Mercedes",
+            correct: false
+        },
+    ]
+},
+{
+    question: "What cricketing term denotes a batsman being dismissed with a score of zero?",
+    answers: [{
+            text: "Duck",
+            correct: true
+        },
+        {
+            text: "Bye",
+            correct: false
+        },
+        {
+            text: "Beamer",
+            correct: false
+        },
+        {
+            text: "Carry",
+            correct: false
+        },
+    ]
+},
+{
+    question: "Who was the British professional wrestler Shirley Crabtree better known as?",
+    answers: [{
+            text: "Big Daddy",
+            correct: true
+        },
+        {
+            text: "Giant Haystacks",
+            correct: false
+        },
+        {
+            text: "Kendo Nagasaki",
+            correct: false
+        },
+        {
+            text: "Masambula",
+            correct: false
+        },
+    ]
+},
+{
+    question: "A stimpmeter measures the speed of a ball over what surface?",
+    answers: [{
+            text: "Golf Putting Green",
+            correct: true
+        },
+        {
+            text: "Football Pitch",
+            correct: false
+        },
+        {
+            text: "Cricket Outfield",
+            correct: false
+        },
+        {
+            text: "Pinball Table",
+            correct: false
+        },
+    ]
+},
+{
+    question: `Which Formula One driver was nicknamed "The Professor`,
+    answers: [{
+            text: "Alain Prost",
+            correct: true
+        },
+        {
+            text: "Ayrton Senna",
+            correct: false
+        },
+        {
+            text: "Niki Lauda",
+            correct: false
+        },
+        {
+            text: "Emerson Fittipaldi",
+            correct: false
+        },
+    ]
+},
+
+
+
 ]
