ab62c2e32c2d6fa861d667c9476bb0ab6eacb21b
henrysevern <henrysevern2@gmail.com>
2022-03-04 11:17:51 +0000

    Format all files

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,165 +1,165 @@
 const questions = [{
-    question: "At which bridge does the annual Oxford and Cambridge boat race start?",
-    answers: [{
-            text: "Hammersmith",
-            correct: false
-        },
-        {
-            text: "Vauxhall ",
-            correct: false
-        },
-        {
-            text: "Putney",
-            correct: true
-        },
-        {
-            text: "Battersea",
-            correct: false
-        },
-    ]
-},
+        question: "At which bridge does the annual Oxford and Cambridge boat race start?",
+        answers: [{
+                text: "Hammersmith",
+                correct: false
+            },
+            {
+                text: "Vauxhall ",
+                correct: false
+            },
+            {
+                text: "Putney",
+                correct: true
+            },
+            {
+                text: "Battersea",
+                correct: false
+            },
+        ]
+    },
 
-{
-    question: "With which team did Michael Schumacher make his Formula One debut at the 1991 Belgian Grand Prix?",
-    answers: [{
-            text: "Jordan",
-            correct: true
-        },
-        {
-            text: "Benetton",
-            correct: false
-        },
-        {
-            text: "Ferrari",
-            correct: false
-        },
-        {
-            text: "Mercedes",
-            correct: false
-        },
-    ]
-},
-{
-    question: "What cricketing term denotes a batsman being dismissed with a score of zero?",
-    answers: [{
-            text: "Bye",
-            correct: false
-        },
-        {
-            text: "Beamer",
-            correct: false
-        },
-        {
-            text: "Carry",
-            correct: false
-        },
-        {
-            text: "Duck",
-            correct: true
-        },
-    ]
-},
-{
-    question: "Who was the British professional wrestler Shirley Crabtree better known as?",
-    answers: [{
-            text: "Giant Haystacks",
-            correct: false
-        },
-        {
-            text: "Big Daddy",
-            correct: true
-        },
-        {
-            text: "Kendo Nagasaki",
-            correct: false
-        },
-        {
-            text: "Masambula",
-            correct: false
-        },
-    ]
-},
-{
-    question: "A stimpmeter measures the speed of a ball over what surface?",
-    answers: [{
-            text: "Golf Putting Green",
-            correct: true
-        },
-        {
-            text: "Football Pitch",
-            correct: false
-        },
-        {
-            text: "Cricket Outfield",
-            correct: false
-        },
-        {
-            text: "Pinball Table",
-            correct: false
-        },
-    ]
-},
-{
-    question: `Which Formula One driver was nicknamed "The Professor"?`,
-    answers: [{
-            text: "Ayrton Senna",
-            correct: false
-        },
-        {
-            text: "Niki Lauda",
-            correct: false
-        },
-        {
-            text: "Alain Prost",
-            correct: true
-        },
-        {
-            text: "Emerson Fittipaldi",
-            correct: false
-        },
-    ]
-},
-
-{
-question: "How many scoring zones are there on a conventional dart board?",
-answers: [{
-        text: "82",
-        correct: true
+    {
+        question: "With which team did Michael Schumacher make his Formula One debut at the 1991 Belgian Grand Prix?",
+        answers: [{
+                text: "Jordan",
+                correct: true
+            },
+            {
+                text: "Benetton",
+                correct: false
+            },
+            {
+                text: "Ferrari",
+                correct: false
+            },
+            {
+                text: "Mercedes",
+                correct: false
+            },
+        ]
     },
     {
-        text: "62",
-        correct: false
+        question: "What cricketing term denotes a batsman being dismissed with a score of zero?",
+        answers: [{
+                text: "Bye",
+                correct: false
+            },
+            {
+                text: "Beamer",
+                correct: false
+            },
+            {
+                text: "Carry",
+                correct: false
+            },
+            {
+                text: "Duck",
+                correct: true
+            },
+        ]
     },
     {
-        text: "102",
-        correct: false
+        question: "Who was the British professional wrestler Shirley Crabtree better known as?",
+        answers: [{
+                text: "Giant Haystacks",
+                correct: false
+            },
+            {
+                text: "Big Daddy",
+                correct: true
+            },
+            {
+                text: "Kendo Nagasaki",
+                correct: false
+            },
+            {
+                text: "Masambula",
+                correct: false
+            },
+        ]
     },
     {
-        text: "42",
-        correct: false
+        question: "A stimpmeter measures the speed of a ball over what surface?",
+        answers: [{
+                text: "Golf Putting Green",
+                correct: true
+            },
+            {
+                text: "Football Pitch",
+                correct: false
+            },
+            {
+                text: "Cricket Outfield",
+                correct: false
+            },
+            {
+                text: "Pinball Table",
+                correct: false
+            },
+        ]
     },
-]
-},
+    {
+        question: `Which Formula One driver was nicknamed "The Professor"?`,
+        answers: [{
+                text: "Ayrton Senna",
+                correct: false
+            },
+            {
+                text: "Niki Lauda",
+                correct: false
+            },
+            {
+                text: "Alain Prost",
+                correct: true
+            },
+            {
+                text: "Emerson Fittipaldi",
+                correct: false
+            },
+        ]
+    },
 
-{
-    question: "In a game of snooker, what colour ball is worth 3 points?",
-    answers: [{
-            text: "Yellow",
-            correct: false
-        },
-        {
-            text: "Brown",
-            correct: false
-        },
-        {
-            text: "Green",
-            correct: true
-        },
-        {
-            text: "Blue",
-            correct: false
-        },
-    ]
+    {
+        question: "How many scoring zones are there on a conventional dart board?",
+        answers: [{
+                text: "82",
+                correct: true
+            },
+            {
+                text: "62",
+                correct: false
+            },
+            {
+                text: "102",
+                correct: false
+            },
+            {
+                text: "42",
+                correct: false
+            },
+        ]
+    },
+
+    {
+        question: "In a game of snooker, what colour ball is worth 3 points?",
+        answers: [{
+                text: "Yellow",
+                correct: false
+            },
+            {
+                text: "Brown",
+                correct: false
+            },
+            {
+                text: "Green",
+                correct: true
+            },
+            {
+                text: "Blue",
+                correct: false
+            },
+        ]
     },
 
     {
@@ -181,28 +181,28 @@ answers: [{
                 correct: true
             },
         ]
-        },
+    },
 
-        {
-            question: "The F1 season of 1994 is remembered for what tragic event?",
-            answers: [{
-                    text: "The Showdown (Australia)",
-                    correct: false
-                },
-                {
-                    text: "Death of Ayrton Senna (San Marino)",
-                    correct: true
-                },
-                {
-                    text: "Verstappen on Fire (Germany)",
-                    correct: false
-                },
-                {
-                    text: `Schumacher's Ban (Britain)`,
-                    correct: false
-                },
-            ]
-            },   
+    {
+        question: "The F1 season of 1994 is remembered for what tragic event?",
+        answers: [{
+                text: "The Showdown (Australia)",
+                correct: false
+            },
+            {
+                text: "Death of Ayrton Senna (San Marino)",
+                correct: true
+            },
+            {
+                text: "Verstappen on Fire (Germany)",
+                correct: false
+            },
+            {
+                text: `Schumacher's Ban (Britain)`,
+                correct: false
+            },
+        ]
+    },
 
 
 
