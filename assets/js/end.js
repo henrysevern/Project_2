418355e020f42b2d15f7ec0adfd40983ac9e0cfc
henrysevern <henrysevern2@gmail.com>
2022-03-08 00:13:13 +0000

    Fix bug with score not uploading to local storage

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,33 +1,31 @@
 const username = document.querySelector('#username');
 const saveScore = document.querySelector('#saveScoreButton');
 const finalScore = document.querySelector('#finalScore');
-const mostRecentScore = localStorage.getItem('score');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const maxHighScores = 4
-
-finalScore.innerText = mostRecentScore
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
     saveScore.disabled = !username.value
-})
+});
 
 saveHighScore = e => {
-    e.preventDefault()
+    e.preventDefault();
 
     const endScore = {
         score: mostRecentScore,
         name: username.value
-    }
+    };
 
     highScores.push(endScore)
     highScores.sort((a,b) => {
         return b.endScore - a.endScore
-    })
+    });
 
-    highScores.splice(4)
+    highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 }
