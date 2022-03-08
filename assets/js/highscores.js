16d856a762c83479f2797f2b2d8539c44e23f1fc
henrysevern <henrysevern2@gmail.com>
2022-03-08 00:11:33 +0000

    Add comments

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,8 +1,11 @@
 // Javascript for highscores.html
 
 const highScoresList = document.querySelector('#highScoresList');
+
+// highScores variable retrives 'highscore' from Local Storage using JSON.parse() to convert the string to an object.
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
+// Creates a list element for the leaderboard using the score and the saved name.
 highScoresList.innerHTML = highScores.map(score => {
     return `<li class=high-score>${score.name} - ${score.score}</li>`
 }).join('');
